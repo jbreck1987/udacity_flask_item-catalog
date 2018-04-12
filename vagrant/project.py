3f985b5ffcf4bd04bbd08035bace1b2521356dab
Josh Breckenridge <bloo1987@gmail.com>
2018-04-12 15:02:53 -0400

    Added test code to remaining routes

--- vagrant/project.py
+++ vagrant/project.py
@@ -4,35 +4,79 @@ from flask import Flask, request
 # instantiate app as Flask instance
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/restaurants/')
 def list_restaurants():
     return 'This is the GET response from list_restaurants!'
 
-@app.route('/restaurant/add_restaurant', methods=['GET', 'POST'])
+
+@app.route('/restaurant/add_restaurant/',
+           methods=['GET', 'POST'])
 def add_restaurant():
     if request.method == 'POST':
         return 'This is the POST response from add_restaurant!'
     return 'This is the GET response from add_restaurant!'
 
-def edit_restaurant():
-    pass
 
-def delete_restaurant():
-    pass
+@app.route('/restaurant/<int:restaurant_id>/edit_restaurant/',
+           methods=['GET', 'POST'])
+def edit_restaurant(restaurant_id):
+    if request.method == 'POST':
+        return '''This is the POST response from
+                 edit_restaurant for ID {}!'''.format(restaurant_id)
+    return '''This is the GET response from
+              edit_restaurant for ID {}!'''.format(restaurant_id)
 
-def list_menu_items():
-    pass
 
-def add_menu_item():
-    pass
+@app.route('/restaurant/<int:restaurant_id>/delete_restaurant/',
+           methods=['GET', 'POST'])
+def delete_restaurant(restaurant_id):
+    if request.method == 'POST':
+        return '''This is the POST response from
+                 delete_restaurant for ID {}!'''.format(restaurant_id)
+    return '''This is the GET response from
+              delete_restaurant for ID {}!'''.format(restaurant_id)
 
-def edit_menu_item():
-    pass
 
-def delete_menu_item():
-    pass
+@app.route('/restaurant/<int:restaurant_id>/list_menu/')
+def list_menu_items(restaurant_id):
+    return '''This is the GET response from
+              list_menu_items for ID {}!'''.format(restaurant_id)
 
+
+@app.route('/restaurant/<int:restaurant_id>/add_item/',
+           methods=['GET', 'POST'])
+def add_menu_item(restaurant_id):
+    if request.method == 'POST':
+        return '''This is the POST response from
+                 add_menu_item for ID {}!'''.format(restaurant_id)
+    return '''This is the GET response from
+              add_menu_item for ID {}!'''.format(restaurant_id)
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:item_id>/delete_item',
+           methods=['GET', 'POST'])
+def edit_menu_item(restaurant_id, item_id):
+    if request.method == 'POST':
+        return '''This is the POST response from
+                 edit_menu_item for rest ID {} and item ID {}!
+                 '''.format(restaurant_id, item_id)
+    return '''This is the GET response from
+              edit_menu_item for rest ID {}! and
+              item ID {}'''.format(restaurant_id, item_id)
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:item_id>/delete_item',
+           methods=['GET', 'POST'])
+def delete_menu_item(restaurant_id, item_id):
+    if request.method == 'POST':
+        return '''This is the POST response from
+                 delete_menu_item for rest ID {} and item ID {}!
+                 '''.format(restaurant_id, item_id)
+    return '''This is the GET response from
+              delete_menu_item for rest ID {}! and
+              item ID {}'''.format(restaurant_id, item_id)
 
 
 if __name__ == '__main__':
