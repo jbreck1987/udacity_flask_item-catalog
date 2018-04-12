e61c6e16e6e3e3c3c35ba4fb987b178defdb1e2c
Josh Breckenridge <bloo1987@gmail.com>
2018-04-12 14:23:20 -0400

    Notated functions, added test to list_restaurants route

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,12 +1,35 @@
 from database_setup import MenuItem, Restaurant, create_db_session
-from flask import Flask
+from flask import Flask, request
 
 # instantiate app as Flask instance
 app = Flask(__name__)
 
-@app.route('/test/')
-def test():
-    return 'app has been instantiated, server is active'
+@app.route('/')
+@app.route('/restaurants/')
+def list_restaurants():
+    return 'This is the GET response from list_restaurants!'
+
+def add_restaurant():
+    pass
+
+def edit_restaurant():
+    pass
+
+def delete_restaurant():
+    pass
+
+def list_menu_items():
+    pass
+
+def add_menu_item():
+    pass
+
+def edit_menu_item():
+    pass
+
+def delete_menu_item():
+    pass
+
 
 
 if __name__ == '__main__':
